5136cddbfc6e2d88fb12221df1c8313a4a800d08
kaelzubs <donmart4u@gmail.com>
2020-11-15 05:01:11 +0100

    initial

--- cookielaw/views.py
+++ cookielaw/views.py
@@ -1,20 +1,11 @@
 from django.shortcuts import render
-from django.db.models import Q
 from . models import cookie_Page
 # Create your views here.
 
 def cookie_policy(request):
-    pages = cookie_Page.objects.order_by('-created')
+    pages = cookie_Page.objects.order_by('-id')
     if request.user.is_staff or request.user.is_superuser:
-        pages = cookie_Page.objects.all().order_by('-created')
+        pages = cookie_Page.objects.all().order_by('-id')
+        
 
-    query = request.GET.get('q')
-    if query:
-        pages = cookie_Page.objects.filter(
-            Q(title__icontains=query) |
-            Q(bodytext__icontains=query) |
-            Q(created__icontains=query)
-        ).distinct()
-
-
-    return render(request, 'cookielaw/cookie_page.html', {'forms': forms, 'pages': pages})
+    return render(request, 'cookielaw/cookie_page.html', {'pages': pages})

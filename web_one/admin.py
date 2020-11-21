9a0d5deba051de311ef3d318a5e2b2aed9fcc51d
kaelzubs <donmart4u@gmail.com>
2020-11-21 02:24:28 +0100

    Initial

--- web_one/admin.py
+++ web_one/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import HomeModel, TrendModel, RelatedModel
+from .models import HomeModel, TrendModel, RelatedModel, Accessories, Nintendo, Xbox, Playstation
 
 # Register your models here.
 admin.site.register(HomeModel)
 admin.site.register(TrendModel)
 admin.site.register(RelatedModel)
+admin.site.register(Accessories)
+admin.site.register(Nintendo)
+admin.site.register(Xbox)
+admin.site.register(Playstation)
+
--- web_one/models.py
+++ web_one/models.py
@@ -35,6 +35,44 @@ class RelatedModel(models.Model):
         return self.rtitle
 
 
+class Accessories(models.Model):
+    atitle = models.CharField(max_length=120)
+    aimage = models.ImageField()
+    aprice = models.CharField(max_length=50)
+    alink = models.URLField()
+    
+    def __str__(self):
+        return self.atitle
+    
+class Nintendo(models.Model):
+    ntitle = models.CharField(max_length=120)
+    nimage = models.ImageField()
+    nprice = models.CharField(max_length=50)
+    nlink = models.URLField()
+    
+    def __str__(self):
+        return self.ntitle
+    
+class Xbox(models.Model):
+    xtitle = models.CharField(max_length=120)
+    ximage = models.ImageField()
+    xprice = models.CharField(max_length=50)
+    xlink = models.URLField()
+    
+    def __str__(self):
+        return self.xtitle
+
+
+class Playstation(models.Model):
+    ptitle = models.CharField(max_length=120)
+    pimage = models.ImageField()
+    pprice = models.CharField(max_length=50)
+    plink = models.URLField()
+    
+    def __str__(self):
+        return self.ptitle
+
+
 class ContactModel(models.Model):
     name = models.CharField(max_length=60)
     email = models.EmailField()
